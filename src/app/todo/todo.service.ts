d4f966b8af1985f30d0551dfdb9c0cb4b43334f6
valerialunaorla <valeria.luna@orla.tech>
2024-06-04 18:12:49 -0300

    feat: cria a entity e o service

--- src/app/todo/todo.service.ts
+++ src/app/todo/todo.service.ts
@@ -1,4 +1,40 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { TodoEntity } from './entity/todo.entity';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  constructor(
+    @InjectRepository(TodoEntity)
+    private readonly todoRepository: Repository<TodoEntity>,
+  ) {}
+
+  async findAll() {
+    return await this.todoRepository.find();
+  }
+
+  async findOneOrFail(id: string) {
+    try {
+      return await this.todoRepository.findOneBy({ id: id });
+    } catch (error) {
+      throw new NotFoundException(error.message);
+    }
+  }
+
+  async create(data) {
+    return await this.todoRepository.save(this.todoRepository.create(data));
+  }
+
+  async update(id: string, data) {
+    const todo = await this.findOneOrFail(id);
+
+    this.todoRepository.merge(todo, data);
+    return await this.todoRepository.save(todo);
+  }
+
+  async deleteById(id: string) {
+    await this.findOneOrFail(id);
+    await this.todoRepository.softDelete(id);
+  }
+}
