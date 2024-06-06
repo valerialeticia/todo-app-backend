234e4c87457f5b3981730fb35f2293f54164c488
valerialunaorla <valeria.luna@orla.tech>
2024-06-06 12:03:32 -0300

    chore: instala e configura swagger

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -13,27 +13,46 @@ import {
 import { TodoService } from './todo.service';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('api/v1/todos')
+@ApiTags('todos')
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get() // endpoint do tipo get, no qual vai retornar todos os todos.
+  @ApiOperation({ summary: 'Listar todas as tarefas' }) // adiciona descrição para a api
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de tarefas retornada com sucesso',
+  }) // documenta sobre a response do endpoint
   async index() {
     return await this.todoService.findAll();
   }
 
   @Post() // endpoint do tipo post, no qual irá criar o todo.
+  @ApiOperation({ summary: 'Criar uma nova tarefa' })
+  @ApiResponse({ status: 201, description: 'Nova tarefa criada com sucesso' })
+  @ApiResponse({ status: 400, description: 'Parâmetros inválidos' })
   async create(@Body() body: CreateTodoDto) {
     return await this.todoService.create(body);
   }
 
   @Get(':id') // endpoint também do tipo get mas que irá precisar passar um id para resgatar o todo oferecido.
+  @ApiOperation({ summary: 'Exibir os dados de uma tarefa' })
+  @ApiResponse({
+    status: 200,
+    description: 'Dados de uma tarefa retornados com sucesso',
+  })
+  @ApiResponse({ status: 404, description: 'Tarefa não foi encontrada' })
   async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.todoService.findOneOrFail(id);
   }
 
   @Put(':id') // endpoint do tipo put, que irá receber um id e precisar de um payload para assim atualizar o todo.
+  @ApiOperation({ summary: 'Atualizar os dados de uma tarefa' })
+  @ApiResponse({ status: 200, description: 'Tarefa atualizada com sucesso' })
+  @ApiResponse({ status: 404, description: 'Tarefa não foi encontrada' })
   async update(
     @Param('id', new ParseUUIDPipe()) id: string,
     @Body() body: UpdateTodoDto,
@@ -42,6 +61,9 @@ export class TodoController {
   }
 
   @Delete(':id') // endpoint do tipo delete que irá precisar de um id para deletar o todo.
+  @ApiOperation({ summary: 'Remover uma tarefa' })
+  @ApiResponse({ status: 204, description: 'Tarefa removida com sucesso' })
+  @ApiResponse({ status: 404, description: 'Tarefa não foi encontrada' })
   @HttpCode(HttpStatus.NO_CONTENT) // aqui irá forçar qual código HTTP irá retornar para o client, podendo receber 204 ou o enum oclocado.
   async destroy(@Param('id', new ParseUUIDPipe()) id: string) {
     await this.todoService.deleteById(id);
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TODOApp API')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document); // endereço da doc swagger
+
   app.useGlobalPipes(new ValidationPipe()); // definindo um validation pipe para todos os endpoints e controllers.
   await app.listen(3000);
 }
